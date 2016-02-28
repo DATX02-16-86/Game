1ea77013676e3825f2ae2664cf7eb5753a946495
Le-Chiffre <rimmer.motzheim@gmail.com>
2016-02-28 23:03:54 +0100

    Added Generator to game project

--- Plugins/VoxelWorld/Source/VoxelWorld/Classes/VoxelTerrain.h
+++ Plugins/VoxelWorld/Source/VoxelWorld/Classes/VoxelTerrain.h
@@ -3,12 +3,15 @@
 #pragma once
 
 #include <Engine.h>
+#include <World/World.h>
 #include "VoxelTerrain.generated.h"
 
 UCLASS()
 class AVoxelTerrain : public AActor
 {
 	GENERATED_BODY()
+
+	generator::World world;
 	
 public:	
 	// Sets default values for this actor's properties
--- Plugins/VoxelWorld/Source/VoxelWorld/VoxelWorld.Build.cs
+++ Plugins/VoxelWorld/Source/VoxelWorld/VoxelWorld.Build.cs
@@ -1,13 +1,20 @@
 // Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
+using System.IO;
 
 public class VoxelWorld : ModuleRules
 {
+    private string ModulePath
+    {
+        get { return Path.GetDirectoryName(RulesCompiler.GetModuleFilename(this.GetType().Name)); }
+    }
+
 	public VoxelWorld(TargetInfo Target)
 	{
 		PublicIncludePaths.AddRange(new string[] { "VoxelWorld/Public" });
-		PrivateIncludePaths.AddRange(new string[] { "VoxelWorld/Private" } );
+		PrivateIncludePaths.AddRange(new string[] { "VoxelWorld/Private", "../../../../Code/Tritium/Code/Core", "../../../../Code/Generator/Code" } );
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "RHI", "RenderCore", "ShaderCore" } );
-	}
+        PublicAdditionalLibraries.AddRange(new string[] { Path.Combine(ModulePath, "../Generator/Generator.lib"), Path.Combine(ModulePath, "../Tritium/TritiumCore.lib") });
+    }
 }
