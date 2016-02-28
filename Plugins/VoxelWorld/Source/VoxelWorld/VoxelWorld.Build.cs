9d5855f21129e8445578882f0665a230f5795c54
Rimmer <rimmer.motzheim@gmail.com>
2016-02-28 23:31:07 +0100

    Added OSX build settings

--- Plugins/VoxelWorld/Source/VoxelWorld/VoxelWorld.Build.cs
+++ Plugins/VoxelWorld/Source/VoxelWorld/VoxelWorld.Build.cs
@@ -15,6 +15,14 @@ public class VoxelWorld : ModuleRules
 		PublicIncludePaths.AddRange(new string[] { "VoxelWorld/Public" });
 		PrivateIncludePaths.AddRange(new string[] { "VoxelWorld/Private", "../../../../Code/Tritium/Code/Core", "../../../../Code/Generator/Code" } );
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "RHI", "RenderCore", "ShaderCore" } );
-        PublicAdditionalLibraries.AddRange(new string[] { Path.Combine(ModulePath, "../Generator/Generator.lib"), Path.Combine(ModulePath, "../Tritium/TritiumCore.lib") });
+
+		if ((Target.Platform == UnrealTargetPlatform.Win64) || (Target.Platform == UnrealTargetPlatform.Win32))
+		{
+			PublicAdditionalLibraries.AddRange(new string[] { Path.Combine(ModulePath, "../Generator/Generator.lib"), Path.Combine(ModulePath, "../Tritium/TritiumCore.lib") });
+		}
+		else
+		{
+			PublicAdditionalLibraries.AddRange(new string[] { Path.Combine(ModulePath, "../Generator/libGenerator.a"), Path.Combine(ModulePath, "../Tritium/libTritiumCore.a") });
+		}
     }
 }
